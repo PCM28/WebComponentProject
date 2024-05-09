4c72a27f5aa1a455b820c8a98b386d29039f9a42
PCM28 <104649488+PCM28@users.noreply.github.com>
2024-05-09 21:17:17 +0200

    updated

--- components/AlertMessage.js
+++ components/AlertMessage.js
@@ -1,40 +1,26 @@
 class AlertMessage extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = `
-      <style>
-        :host {
-          display: block;
-          padding: 10px;
-          margin-bottom: 10px;
-          border-radius: 5px;
-        }
-        .success { background-color: #5cb85c; color: #fff; }
-        .warning { background-color: #f0ad4e; color: #fff; }
-        .error { background-color: #d9534f; color: #fff; }
-        .info { background-color: #5bc0de; color: #fff; }
-      </style>
-      <div class="alert"></div>
-    `;
-  }
+    constructor() {
+      super();
+      this.type = ""; //Variable 1 de Alert
+      this.message = ""; //Variable 2 de alert
 
-  static get observedAttributes() {
-    return ['type', 'message'];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'type') {
-      this.shadowRoot.querySelector('.alert').className = newValue;
-    } else if (name === 'message') {
-      if (!newValue) {
-        this.style.display = 'none';
-      } else {
-        this.style.display = 'block';
-        this.shadowRoot.querySelector('.alert').textContent = newValue;
-      }
+      this.attachShadow( { mode: "open" } );
+      this.shadowRoot.innerHTML = `
+        <style>
+          :host {
+            display: flex;
+          }
+          
+        </style>
+        <div class="alert">
+          <h1>Alert</h1>
+          <p>Message</p>
+        </div>
+      `;
     }
-  }
-}
 
-customElements.define('alert-message', AlertMessage);
+    //Código restante
+
+  }
+  
+  customElements.define('alert-message', AlertMessage);  
